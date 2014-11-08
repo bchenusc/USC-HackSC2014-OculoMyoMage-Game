6a074fe9d3b0ad126dc8bc542405372c8bed5751
David Zhang <davidtzh@usc.edu>
2014-11-08 11:41:36 -0800

    Sword collisions work, basic enemy movement

---
+++ LoneSword/Assets/Scripts/DavidScripts/EnemyMovement.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	GameObject player;
+	bool canMove = true;
+
+	// Use this for initialization
+	void Awake () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.LookAt(player.transform.position);
+		float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+		if(distanceToPlayer > 5f && canMove)
+		{
+			rigidbody.velocity = transform.forward.normalized * 3;
+		}
+		else if (distanceToPlayer < 4f)
+		{
+			rigidbody.velocity = Vector3.zero;
+			canMove = false;
+		}
+	}
+}
---
+++ LoneSword/Assets/Scripts/DavidScripts/SwordController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using Pose = Thalmic.Myo.Pose;
+using VibrationType = Thalmic.Myo.VibrationType;
+
+public class SwordController : MonoBehaviour {
+
+	// Myo game object to connect with.
+	// This object must have a ThalmicMyo script attached.
+	public GameObject myo = null;
+
+	public GameObject player = null;
+	
+	void Awake()
+	{
+		myo = GameObject.Find ("Myo");
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider collision) {
+		if(collision.gameObject.CompareTag("EnemySword"))
+		{
+			Debug.Log(Quaternion.Angle(player.transform.rotation, collision.transform.rotation));
+			player.rigidbody.AddForce(player.transform.forward * 50f);
+		}
+	}
+}
