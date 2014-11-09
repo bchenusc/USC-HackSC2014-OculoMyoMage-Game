49ce690bc754521b1b470abe20dadf5230b0bf73
Mr Fluffy Sheep <brianych@usc.edu>
2014-11-08 16:56:15 -0800

    spawning

---
+++ LoneSword/Assets/Scripts/Health/Health.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public int HP = 10;
+
+	void TakeDamage()
+	{
+		HP -= 1;
+		if (HP == 0)
+		{
+			Application.LoadLevel(0);
+		}
+	}
+}
---
+++ LoneSword/Assets/Scripts/Spawner/SkeleSpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkeleSpawner : MonoBehaviour {
+
+	public Transform prefab;
+	Transform player;
+
+	void Start()
+	{
+		player = GameObject.Find ("OVRPlayerController").transform;
+		SingletonObject.Get.getTimer ().Add (gameObject.GetInstanceID () + "spawn", Spawn, Random.Range (5.0f, 10.0f), true);
+	}
+
+	void Spawn()
+	{
+		Instantiate (prefab, transform.position, Quaternion.identity);
+	}
+}
